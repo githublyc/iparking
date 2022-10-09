49cf3ad8c1827dffc7abd637a86dfa64afae9e19
lyc <1165893118@qq.com>
2022-10-09 15:00:42 +0800

    新建项目

---
+++ src/main/java/com/example/iparking/IparkingApplication.java
@@ -0,0 +1,13 @@
+package com.example.iparking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IparkingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IparkingApplication.class, args);
+	}
+
+}
